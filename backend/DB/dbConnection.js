e0774fff0ae5b17b8734c87ba5af57bbc7b57b9c
Ahmed Toba <ahmdtwbt22@gmail.com>
2025-10-06 22:56:14 +0300

    Fix DB Connection

--- backend/DB/dbConnection.js
+++ backend/DB/dbConnection.js
@@ -4,11 +4,10 @@ const dbConnection = async () => {
     try {
         // For development, you can use a local MongoDB connection
         // Replace this with your actual MongoDB connection string
-        const DB_URI =
-            process.env.DATABASE_URI.replace(
-                "<password>",
-                process.env.DATABASE_PASSWORD
-            ) || "mongodb://localhost:27017/learningManagementSystem";
+        const DB_URI = process.env.DATABASE_URI.replace(
+            "<db_password>",
+            process.env.DATABASE_PASSWORD
+        );
 
         await mongoose.connect(DB_URI);
         console.log("Database connected successfully");
